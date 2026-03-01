08bb8e0016f7938a7f157946fc091983a9c6bf27
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-02-28 21:45:33 -0500

    DTO que almacena los datos necesarios para operar sobre el stock de un producto

---
+++ src/main/java/com/electrodostore/producto_service/dto/ProductoOperacionStockDto.java
@@ -0,0 +1,17 @@
+package com.electrodostore.producto_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter  @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+/*DTO cuya función será almacenar los datos relevantes cuando se vaya a hacer una operación (descontar, reponer, validar, etc.)
+ sobre el stock de un determinado producto */
+public class ProductoOperacionStockDto {
+
+    private Long productoId;
+    private Long cantidadOperar; //Cantidad que se va a operar sobre el stock del producto
+}
